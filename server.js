5c99c786076329ea3ffe1e890f2974ebbc10bfb1
Gleb <maikezseller@gmail.com>
2021-09-10 21:54:10 +0300

    define all actions and two function on server

--- server.js
+++ server.js
@@ -4,7 +4,24 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
+const ACTIONS = require('./src/socket/actions');
 const PORT = process.env.PORT || 3001;
+
+function getClientRooms() {
+    const { rooms } = io.sockets.adapter;
+
+    return Array.from(rooms.keys());
+}
+
+function shareRoomsInfo() {
+    io.emit(ACTIONS.SHARE_ROOMS, {
+        rooms: getClientRooms(),
+    });
+}
+
+io.on('connection', (socket) => {
+    shareRoomsInfo();
+});
 
 server.listen(PORT, () => {
     console.log(`Server was started on port ${PORT}`);
