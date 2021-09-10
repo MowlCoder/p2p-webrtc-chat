e981d3b7c2049a835c9a29623dbf3debc9646fdb
Gleb <maikezseller@gmail.com>
2021-09-10 22:05:17 +0300

    add handler to JOIN and LEAVE

--- server.js
+++ server.js
@@ -21,6 +21,57 @@ function shareRoomsInfo() {
 
 io.on('connection', (socket) => {
     shareRoomsInfo();
+
+    socket.on(ACTIONS.JOIN, (config) => {
+        const { room: roomID } = config;
+        const { rooms: joinedRooms } = socket;
+
+        if (Array.from(joinedRooms).includes(roomID)) {
+            return console.warn(`Already joined to ${roomID}`);
+        }
+
+        const clients = Array.from(io.sockets.adapter.rooms.get(roomID) || []);
+
+        clients.forEach((clientID) => {
+            io.to(clientID).emit(ACTIONS.ADD_PEER, {
+                peerID: socket.id,
+                createOffer: false,
+            });
+
+            socket.emit(ACTIONS.ADD_PEER, {
+                peerID: clientID,
+                createOffer: true,
+            });
+        });
+
+        socket.join(roomID);
+        shareRoomsInfo();
+    });
+
+    function leaveRoom() {
+        const { rooms } = socket;
+
+        Array.from(rooms).forEach((roomID) => {
+            const clients = Array.from(io.sockets.adapter.rooms.get(roomID) || []);
+
+            clients.forEach((clientID) => {
+                io.to(clientID).emit(ACTIONS.REMOVE_PEER, {
+                    peerID: socket.id,
+                });
+
+                socket.emit(ACTIONS.REMOVE_PEER, {
+                    peerID: clientID,
+                });
+            });
+
+            socket.leave(roomID);
+        });
+
+        shareRoomsInfo();
+    }
+
+    socket.on(ACTIONS.LEAVE, leaveRoom);
+    socket.on('disconnect', leaveRoom);
 });
 
 server.listen(PORT, () => {
