278ab9cfa279081285e9a71217d95c35c93287bc
Gleb <maikezseller@gmail.com>
2021-09-11 16:08:13 +0300

    change logic to get rooms and setup main lobby

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,37 @@
+import { useEffect, useState } from "react";
 import socket from "../../socket";
+import ACTIONS from "./../../socket/actions";
+import {useHistory} from "react-router";
+import { v4 } from 'uuid';
 
 export default function Main() {
+    const history = useHistory();
+    const [rooms, updateRooms] = useState([]);
+
+    useEffect(() => {
+        socket.on(ACTIONS.SHARE_ROOMS, ({ rooms = [] } = {}) => {
+            updateRooms(rooms);
+        })
+    }, []);
+
     return (
         <div>
-            MAIN PAGE
+            <h1>Available Rooms</h1>
+
+            <ul>{
+                rooms.map((roomID) => (
+                    <li key={roomID}>
+                        {roomID}
+                        <button onClick={() => {
+                            history.push(`/room/${roomID}}`)
+                        }}>Join Room</button>
+                    </li>
+                ))
+            }</ul>
+
+            <button onClick={() => {
+                history.push(`/room/${v4()}`)
+            }}>Create New Room</button>
         </div>
     );
 }
